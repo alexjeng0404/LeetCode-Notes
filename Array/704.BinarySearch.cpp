7a9489ba10507021cc3f9c975459cd336e52e10a
Alex <alexjeng0404@gmail.com>
2025-10-06 22:37:56 +0800

    feat(Array): Update 704.BinarySearch with English comments and main for local testing

--- Array/704.BinarySearch.cpp
+++ Array/704.BinarySearch.cpp
@@ -1,17 +1,28 @@
 class Solution {
     public:
-        int search(vector<int>& nums, int target) {
+        /**
+         * @brief Finds the target in a sorted array using Binary Search (Closed Interval [l, r] approach).
+         * @param nums The sorted array of integers.
+         * @param target The value to search for.
+         * @return The index of the target if found, otherwise -1.
+         */
+        int search(std::vector<int>& nums, int target) {
             int n = nums.size();
             int l = 0;
-            int r = n-1; 
-    
-            while(l<=r){
-                int m = l + floor((r-l)/2);
-                if (nums[m] < target){   //go right
-                    l = m+1;
+            int r = n - 1; 
+            // [l, r] defines the search range where both boundaries are inclusive.
+            // We use l <= r because when l == r, there is still one element to check.
+            while(l <= r){
+                // Calculate the middle index 'm'. This formula prevents integer overflow
+                int m = l + (r - l) / 2; 
+                
+                if (nums[m] < target){  
+                    // Target is on the right side. The element at index m is too small, 
+                    l = m + 1;
                 }
-                else if (nums[m] > target){ //go left
-                    r = m-1;
+                else if (nums[m] > target){ 
+                    // Target is on the left side. The element at index m is too large, 
+                    r = m - 1;
                 }
                 else{
                     return m;
