a5282db53350d7a8a0266fb06cef91f98decdc48
Alex <alexjeng0404@gmail.com>
2025-10-06 22:51:38 +0800

    feat(HashTable): Implement 242. Valid Anagram with map and English notes

---
+++ HashTable/242.ValidAnagram.cpp
@@ -0,0 +1,37 @@
+#include <string>
+#include <unordered_map>
+#include <algorithm> // Not strictly needed for this implementation, but good practice
+
+class Solution {
+public:
+    /**
+     * @brief Checks if string t is an anagram of string s using a Hash Map (Unordered Map).
+     * @param s The first string.
+     * @param t The second string.
+     * @return true if t is an anagram of s, false otherwise.
+     */
+    bool isAnagram(std::string s, std::string t) {
+        if (s.length() != t.length()){
+            return false;
+        }
+        
+        // Use an unordered_map to store the frequency of characters in string s.
+        // Key: character (char), Value: frequency (int).
+        std::unordered_map <char, int> map;
+        
+        for (char ch : s){
+            map[ch] += 1;
+        }
+        
+        for (char ch : t){
+            // if t has a word not in s or t has more same character than s
+            if (map.find(ch) == map.end() || map[ch] == 0){
+                return false;
+            }
+            
+            map[ch] -= 1;
+        }
+        
+        return true;
+    }
+};
