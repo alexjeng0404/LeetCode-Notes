bd3eff17fba335997e582c551bb9641ab3549fdb
Alex <alexjeng0404@gmail.com>
2025-10-08 10:41:10 +0800

    feat(HashTable): Complete 383. Ransom Note (2025-10-07)

---
+++ HashTable/383.RansomNote.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        vector<int> cnt(128, 0);
+        
+        // Count characters in t
+        for (char c : ransomNote) {
+            cnt[c]++;
+        }
+        
+        for (char m : magazine){
+            if (cnt[m]>0) cnt[m] -= 1;
+        }
+        
+        for (char c : ransomNote) {
+            if (cnt[c]!=0) return false;
+        }
+        return true;
+    }
+};
